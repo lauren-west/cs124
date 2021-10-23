c1e5202dc955217e355a4bad391176f9954bf8a7
lauren-west <lwest@hmc.edu>
2021-10-23 13:21:48 -0700

    Revert "delete build and node"
    This reverts commit 1a00137619d0ec88e46465b308d4da1fae97c487.

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,21 +1,6 @@
 import './main.css'
 import React, {useState} from "react";
 import Alert from "./Alert";
-import {useCollection} from "react-firebase-hooks/firestore";
-import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
-import firebase from "firebase/compat";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCd9qqxvMpEKpBzwfWcc2tlRFa6ICaLH_s",
-    authDomain: "hmc-cs124-fa21-labs.firebaseapp.com",
-    projectId: "hmc-cs124-fa21-labs",
-    storageBucket: "hmc-cs124-fa21-labs.appspot.com",
-    messagingSenderId: "949410042946",
-    appId: "1:949410042946:web:0113b139a7e3cd1cc709db"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 
 function ListsDisplay(props) {
     const [showAlert, setShowAlert] = useState(false);
@@ -51,16 +36,6 @@ function ListsDisplay(props) {
 
 function MainPage(props) {
     const [showAlert, setShowAlert] = useState(false);
-    const collectionName = "lwest-hmc-tasks2"
-    const query = db.collection(collectionName)
-    const [value, loading, error] = useCollection(query)
-    const collectionRef = db.collection(collectionName);
-    const data = !loading ? value.docs.map((element)=> element.data()) : []
-   /* const docRef = collectionRef.doc(generateUniqueID())
-
-    docRef.set({
-        created: firebase.database.ServerValue.TIMESTAMP
-    })*/
 
     function handleAlertOK(listName) {
         props.setData([...props.data, {
@@ -74,9 +49,8 @@ function MainPage(props) {
 
     return (
         <>
-            {loading && <p>Loading...</p>}
             <h1 id="MyLists">My Lists</h1>
-            {data.map((x) => <ListsDisplay setData={props.setData} data={data} list={x} onClick={props.onListClick}/>)}
+            {props.data.map((x) => <ListsDisplay setData={props.setData} data={props.data} list={x} onClick={props.onListClick}/>)}
             <div id="button1">
                 <button onClick={() => setShowAlert(true)} className="addList addTask">
                     <img src="plus-solid.svg"/>
